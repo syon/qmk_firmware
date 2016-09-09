3f557ff1f15dc187b1e8803877f0d6d9cb01e965
syon <syon.xiii@gmail.com>
2016-09-09 23:26:49 +0900

    KC_KP_ENTER

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Del  |    | Mo.  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |    |   MV |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'    `-------------+------+------+------+------+--------'
- *   |  FN  |  Win |  Win |  Alt |F13/Ct|                                |F14/FN|  App |   [  |   ]  | Enter|
+ *   |  FN  |  Win |  Win |  Alt |F13/Ct|                                |F14/FN|  App |   [  |   ]  | KpEnt|
  *   `----------------------------------'                                `----------------------------------'
  *                                      ,-------------.    ,-------------.
  *                                      | Left |Right |    | Home | End  |
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(MOUSE),  KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
                     KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         MO(MV),     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-                         LT(FN, KC_F14),    KC_APP,     KC_LBRC,    KC_RBRC,    KC_ENT,
+                         LT(FN, KC_F14),    KC_APP,     KC_LBRC,    KC_RBRC,    KC_KP_ENTER,
         KC_HOME,    KC_END,
         KC_UP,
         KC_DOWN,    KC_F2,      KC_ENT
